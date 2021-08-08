5fcc1323d66e153931c5f002b6c143606a528c14
AzetaTakuya <azeta0611@gmail.com>
2021-08-09 01:26:03 +0900

    [add] RGBVisualizer

---
+++ Assets/DepthToPointCloud/Scripts/RGBVisualizer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DepthToPointCloud
+{
+    public class RGBVisualizer : MonoBehaviour
+    {
+        [SerializeField] private ImagesHandler Handler;
+        [SerializeField] private RawImage Image;
+
+        private void Update()
+        {
+            if (!Handler.IsStarted) return;
+
+            if(Image.texture == null)
+            {
+                Image.texture = Handler.RGB_Texture;
+            }
+        }
+    }
+
+}
