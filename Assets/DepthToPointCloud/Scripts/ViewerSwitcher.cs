d0f48152fd0af355b631fae43a225fe4395d7335
AzetaTakuya <azeta0611@gmail.com>
2021-08-09 01:46:42 +0900

    [add] ViewerSwitcher

---
+++ Assets/DepthToPointCloud/Scripts/ViewerSwitcher.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AppBase
+{
+    public class ViewerSwitcher : MonoBehaviour
+    {
+        [SerializeField] private GameObject PointCloudViewer;
+        [SerializeField] private GameObject RGBViewer;
+
+        private enum ViewerType
+        {
+            None,
+            PointCloud,
+            RGB
+        }
+        private ViewerType Type = ViewerType.None;
+
+        private void Start()
+        {
+            ChangeView();
+        }
+
+        private void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                ChangeView();
+            }
+        }
+
+        private void ChangeView()
+        {
+            switch(Type)
+            {
+                case ViewerType.None:
+                    SetPointCloud();
+                    break;
+                case ViewerType.PointCloud:
+                    SetRGB();
+                    break;
+                case ViewerType.RGB:
+                    SetPointCloud();
+                    break;
+            }
+
+            void SetPointCloud()
+            {
+                PointCloudViewer.SetActive(true);
+                RGBViewer.SetActive(false);
+                Type = ViewerType.PointCloud;
+            }
+
+            void SetRGB()
+            {
+                PointCloudViewer.SetActive(false);
+                RGBViewer.SetActive(true);
+                Type = ViewerType.RGB;
+            }
+
+        }
+    }
+}
